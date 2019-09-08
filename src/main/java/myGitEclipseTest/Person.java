47b5bfd8e65886450a8520a9d95de33021510270
tom_glb <goodMorning_pro@atguigu.com>
2019-09-08 11:22:20 +0800

    000

--- src/main/java/myGitEclipseTest/Person.java
+++ src/main/java/myGitEclipseTest/Person.java
@@ -3,6 +3,7 @@ package myGitEclipseTest;
 public class Person {
 	public static void main(String[] args) {
 		System.out.println("123");
-		System.out.println("fziiyyppp");
+		System.out.println("fziiyypppooo");
+		System.out.println("fziiyypppyyy");
 	}
 }
