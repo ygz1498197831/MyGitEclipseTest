39d2a158ac98d6d24654a133b753e24e051563cb
tom_glb <goodMorning_pro@atguigu.com>
2019-09-08 11:14:08 +0800

    ppp

--- src/main/java/myGitEclipseTest/Person.java
+++ src/main/java/myGitEclipseTest/Person.java
@@ -3,6 +3,6 @@ package myGitEclipseTest;
 public class Person {
 	public static void main(String[] args) {
 		System.out.println("123");
-		System.out.println("fziiyy");
+		System.out.println("fziiyyuu");
 	}
 }
